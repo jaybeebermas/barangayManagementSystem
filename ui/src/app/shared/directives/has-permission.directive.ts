c0219b907b25e4376114437021ab083fc0e9867f
Lance-zy <lncjsnllgs@gmail.com>
2026-05-25 10:47:29 +0800

    i remove effect-based authorization check in hasPermission directive to use input setter directly

--- ui/src/app/shared/directives/has-permission.directive.ts
+++ ui/src/app/shared/directives/has-permission.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, TemplateRef, ViewContainerRef, inject, effect } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef, inject } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Directive({
@@ -11,25 +11,15 @@ export class HasPermissionDirective {
   private readonly viewContainer = inject(ViewContainerRef);
 
   private hasView = false;
-  private permission = '';
-
-  constructor() {
-    effect(() => {
-      // This effect runs immediately and whenever currentUser changes
-      const user = this.authService.currentUser();
-      const isAuthorized = this.permission ? this.authService.hasPermission(this.permission) : false;
-      
-      if (isAuthorized && !this.hasView) {
-        this.viewContainer.createEmbeddedView(this.templateRef);
-        this.hasView = true;
-      } else if (!isAuthorized && this.hasView) {
-        this.viewContainer.clear();
-        this.hasView = false;
-      }
-    });
-  }
 
   @Input() set hasPermission(permission: string) {
-    this.permission = permission;
+    const isAuthorized = this.authService.hasPermission(permission);
+    if (isAuthorized && !this.hasView) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.hasView = true;
+    } else if (!isAuthorized && this.hasView) {
+      this.viewContainer.clear();
+      this.hasView = false;
+    }
   }
 }
